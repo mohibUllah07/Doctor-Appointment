efa0596f49e72a6dcbf09b9f7779cace4c96f0a4
talibb1 <talibuddinqazi@gmail.com>
2023-10-28 13:42:33 +0500

    All Files is created

---
+++ blank-page.php
@@ -0,0 +1,50 @@
+<?php include "Navbar/header.php" ?>
+			
+			<!-- Breadcrumb -->
+			<div class="breadcrumb-bar">
+				<div class="container-fluid">
+					<div class="row align-items-center">
+						<div class="col-md-12 col-12">
+							<nav aria-label="breadcrumb" class="page-breadcrumb">
+								<ol class="breadcrumb">
+									<li class="breadcrumb-item"><a href="index-2.html">Home</a></li>
+									<li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+								</ol>
+							</nav>
+							<h2 class="breadcrumb-title">Blank Page</h2>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- /Breadcrumb -->
+			
+			<!-- Page Content -->
+			<div class="content">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-12">
+							<h5>Blank Page</h5>
+						</div>
+					</div>
+				</div>
+
+			</div>		
+			<!-- /Page Content -->
+			<?php include "Footer/footer.php" ?>
+		</div>
+		<!-- /Main Wrapper -->
+	  
+		<!-- jQuery -->
+		<script src="assets/js/jquery.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+		<script src="assets/js/popper.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+		
+	</body>
+
+<!-- doccure/blank-page.html  30 Nov 2019 04:12:20 GMT -->
+</html>
